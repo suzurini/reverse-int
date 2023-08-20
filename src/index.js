19c129dde4ed1ad05ce3c867176951fb22de9690
suzurini <valloforr@gmail.com>
2023-08-20 22:36:20 +0300

    fix: toString()

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function reverse (n) {
     let strNewNumber = '';
     for (i = 0; i < digitCount; i++) {
         strNewNumber += n.toString().slice(-1);
-        n = n.toString.slice(0, -1)
+        n = n.toString().slice(0, -1)
     }
     return parseInt(strNewNumber);
 }
